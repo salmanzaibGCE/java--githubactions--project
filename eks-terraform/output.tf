5842e41001390642525b2ba4597f2eec7ac6b811
SALMAN ZAIB <salmanzaib523@gmail.com>
2026-07-24 23:16:12 +0500

    Add EKS cluster outputs to output.tf
    Add outputs for EKS cluster details including name, endpoint, and certificate authority data.

--- eks-terraform/output.tf
+++ eks-terraform/output.tf
@@ -1,1 +1,14 @@
+output "cluster_name" {
+  description = "The name of your EKS Cluster"
+  value       = aws_eks_cluster.main.name
+}
 
+output "cluster_endpoint" {
+  description = "The connection endpoint for your EKS control plane"
+  value       = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "The certificate authority data needed to securely login to Kubernetes"
+  value       = aws_eks_cluster.main.certificate_authority[0].data
+}
